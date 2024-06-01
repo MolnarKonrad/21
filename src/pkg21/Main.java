59e5e2bc2ed36d2cf1e40617b84e231702ef460f
Konrad <molnar.konrad@gmail.com>
2024-06-01 13:31:40 +0200

    kiiras() létrehozva

--- src/pkg21/Main.java
+++ src/pkg21/Main.java
@@ -1,9 +1,26 @@
 package pkg21;
 
 public class Main {
+    
+    private static int[] lapokJ = new int[10];
+    private static int[] lapokG = new int[10];
 
     public static void main(String[] args) {
+        konzolraIr("");
+        jatek();
+    }
 
+    private static void konzolraIr(String kimenet) {
+        System.out.print(kimenet);
+    }
+
+    private static void jatek() {
+        jetekeleje();
+        
+    }
+
+    private static void jetekeleje() {
+        
     }
     
 }
