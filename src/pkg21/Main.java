4d9e2c48e1f68e5c8a6cd3415261f1a17224d034
Konrad <molnar.konrad@gmail.com>
2024-06-01 14:10:24 +0200

    Lapok generálása

--- src/pkg21/Main.java
+++ src/pkg21/Main.java
@@ -7,6 +7,7 @@ public class Main {
     private static int[] lapokJ = new int[10];
     private static int[] lapokG = new int[10];
     static Random rnd = new Random();
+    static int osszeg = 0;
 
     public static void main(String[] args) {
         konzolraIr("");
@@ -22,21 +23,53 @@ public class Main {
         
     }
 
-    private static void jatekeleje() {
+    private static void jatekeleje() {        
+        ElsoLapok();           
+        konzolraIr("A gép lapjai: ");
+        kiirTomb(lapokG);   
         konzolraIr("A játékos lapjai: ");
-        Elso2Lap();
-              
+        kiirTomb(lapokJ);  
     }
 
-    private static void Elso2Lap() {
-        lapok(lapokJ,2);
-        lapok(lapokG,2);
+    private static void ElsoLapok() {
+        GLapjai();
+        while(osszegzes(lapokG) < 17){
+            lapok(lapokG);
+        }
     }
     
-    private static int[] lapok(int[] tomb, int db) {
-        for (int i = 0; i < db; i++) {
-            tomb[i] = rnd.nextInt(2, 11);
+    private static void GLapjai() {
+        
+    }
+    
+    private static int[] lapok(int[] tomb) {
+        for (int i = 0; i < tomb.length; i++) {
+            if(tomb[i] == 0){
+                tomb[i] = rnd.nextInt(2, 11);
+            }else{ i++; }
         }
         return tomb;
     }
+    
+    private static int osszegzes(int[] tomb){
+        int osszeg = 0;
+        for (int i = 0; i < tomb.length; i++) {
+            osszeg += tomb[i];
+        }
+        return osszeg;
+    }
+    
+    private static void ujabbHuzas() {
+        
+    }
+    
+    private static void kiirTomb(int[] tomb) {
+        for (int i = 0; i < tomb.length; i++) {
+            if ((tomb[i] != 0)) {
+                System.out.print(tomb[i] + " ");
+            }
+        }
+        System.out.println("");
+    }   
+    
 }
