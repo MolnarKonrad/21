65f38298b59cb0fec6bc173d8cb180a779b300b0
Konrad <molnar.konrad@gmail.com>
2024-06-01 13:38:20 +0200

    Játékos első 2 lapja

--- src/pkg21/Main.java
+++ src/pkg21/Main.java
@@ -1,26 +1,40 @@
 package pkg21;
 
+import java.util.Random;
+
 public class Main {
     
     private static int[] lapokJ = new int[10];
     private static int[] lapokG = new int[10];
+    static Random rnd = new Random();
 
     public static void main(String[] args) {
         konzolraIr("");
         jatek();
-    }
+    }    
 
     private static void konzolraIr(String kimenet) {
         System.out.print(kimenet);
     }
 
     private static void jatek() {
-        jetekeleje();
+        jatekeleje();
         
     }
 
-    private static void jetekeleje() {
-        
+    private static void jatekeleje() {
+        jElso2Lapja();
+    }
+
+    private static void jElso2Lapja() {
+        lapok(lapokJ,2);
+    }
+    
+    private static int[] lapok(int[] tomb, int db) {
+        for (int i = 0; i < db; i++) {
+            tomb[i] = rnd.nextInt(2, 11);
+        }
+        return tomb;
     }
     
 }
